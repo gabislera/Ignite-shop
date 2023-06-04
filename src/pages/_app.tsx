98a7f55ff6dca1110e0b34d090c91066c9428fba
Gabriela Carniel <gabiicarniel@gmail.com>
2023-06-04 11:19:56 -0300

    added styles and keen slider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,22 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from "../styles/pages/app"
+import Image from "next/image"
+
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <div>
+          <Image src={logoImg} alt="" />
+        </div>
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
-export default MyApp
