c71e5abc4b867723da1e2fdd7814ebe2910c8654
Gabriela Carniel <gabiicarniel@gmail.com>
2023-06-07 13:58:38 -0300

    created checkout api and product page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { globalStyles } from "../styles/global"
 import logoImg from '../assets/logo.svg'
 import { Container, Header } from "../styles/pages/app"
 import Image from "next/image"
+import Link from "next/link"
 
 globalStyles()
 
@@ -11,7 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
     <Container>
       <Header>
         <div>
-          <Image src={logoImg} alt="" />
+          <Link href={'/'}>
+            <Image src={logoImg} alt="" />
+          </Link>
         </div>
       </Header>
 
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,12 +4,13 @@ export const globalStyles = globalCss({
   '*': {
     margin: 0,
     padding: 0,
+    boxSizing: 'border-box',
   },
 
   body: {
     backgroundColor: '$gray900',
     color: '$gray100',
-    '-webkit-font-smoothing': 'antialiased'
+    '-webkit-font-smoothing': 'antialiased',
   },
 
   'body, input, textarea, button': {
